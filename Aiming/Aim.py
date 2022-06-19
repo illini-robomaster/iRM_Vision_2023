9adb65297907c37e528b254f5eb40c42608a918f
RogerQi <17267101+RogerQi@users.noreply.github.com>
2022-06-19 18:39:50 -0500

    Implement a simple aimer class that generates uncalibrated yaw/pitch differences from NN predictions

--- Aiming/Aim.py
+++ Aiming/Aim.py
@@ -1,6 +1,69 @@
+import numpy as np
+import config
+
 class Aim:
     def __init__(self):
         pass
 
-    def get_rotation(self, pred):
-        pass
+    def process_one(self, pred_list, enemy_team, depth_map):
+        assert enemy_team in ['blue', 'red']
+        closet_pred = self.get_closet_pred(pred_list, enemy_team)
+        if closet_pred is None:
+            return None
+        name, confidence, bbox = closet_pred
+        center_x, center_y, width, height = bbox
+
+        # Get yaw/pitch differences in radians
+        yaw_diff, pitch_diff = self.get_rotation_angle(center_x, center_y)
+        upper_left_x = int(center_x - width / 2)
+        upper_left_y = int(center_y - height / 2)
+        lower_right_x = int(center_x + width / 2)
+        lower_right_y = int(center_y + height / 2)
+
+        # Get distance to target
+        depth_region = depth_map[upper_left_y:lower_right_y,upper_left_x:lower_right_x]
+        estimated_depth = np.mean(depth_region[depth_region > 0])
+
+        calibrated_yaw_diff, calibrated_pitch_diff = self.posterior_calibration(yaw_diff, pitch_diff, estimated_depth)
+
+        return (calibrated_yaw_diff, calibrated_pitch_diff)
+    
+    def posterior_calibration(self, yaw_diff, pitch_diff, distance):
+        """Given a set of naively estimated parameters, return calibrated parameters
+        from a range table?
+
+        Args:
+            yaw_diff (float): yaw difference in radians
+            pitch_diff (float): pitch difference in radians
+            distance (float): distance to target in meters
+
+        Returns:
+            (float, float): calibrated yaw_diff, pitch_diff in radians
+        """
+        # TODO: compute a range table
+        return (yaw_diff, pitch_diff)
+    
+    def get_closet_pred(self, pred_list, enemy_team):
+        closet_pred = None
+        closet_l2_dist = None
+        obj_of_interest = [f"armor_{enemy_team}"]
+        for name, conf, bbox in pred_list:
+            # name from C++ string is in bytes; decoding is needed
+            if name.decode('utf-8') not in obj_of_interest: continue
+            center_x, center_y, _, _ = bbox
+            # TODO: use depth distance instead of L2 dist for watcher for easy targets?
+            cur_l2_dist = np.square(config.IMG_CENTER_X // 2 - center_x) + np.square(config.IMG_CENTER_Y // 2 - center_y)
+            if closet_pred is None:
+                closet_pred = (name, conf, bbox)
+                closet_l2_dist = cur_l2_dist
+            else:
+                if cur_l2_dist < closet_l2_dist:
+                    closet_pred = (name, conf, bbox)
+                    closet_l2_dist = cur_l2_dist
+        return closet_pred
+
+    def get_rotation_angle(bbox_center_x, bbox_center_y):
+        yaw_diff = (bbox_center_x - config.IMG_CENTER_X) * (config.RGBD_CAMERA.YAW_FOV_HALF / config.IMG_CENTER_X)
+        pitch_diff = (bbox_center_y - config.IMG_CENTER_Y) * (config.RGBD_CAMERA.PITCH_FOV_HALF / config.IMG_CENTER_Y)
+
+        return yaw_diff, pitch_diff
--- vision.py
+++ vision.py
@@ -1,47 +1,58 @@
 import time
 import cv2
 from Aiming.Aim import Aim
-from Communication.communicator import Communicator, create_packet, send_packet, serial_port
+# from Communication.communicator import serial_port
 from Detection.darknet import Yolo
-from variables import *
+import config
+import numpy as np
+
+def pad_packet(header, seq_num, yaw_offset, pitch_offset):
+    assert header in [b'ST', b'MY']
+    packet = header
+    assert seq_num >= 0 and seq_num < 2**32 - 1 # uint32
+    packet += seq_num.to_bytes(4, 'big')
+    # YAW/PITCH offset should not be too high
+    assert yaw_offset > -config.RGBD_CAMERA.YAW_FOV_HALF and yaw_offset < config.RGBD_CAMERA.YAW_FOV_HALF
+    assert pitch_offset > -config.RGBD_CAMERA.PITCH_FOV_HALF and pitch_offset < config.RGBD_CAMERA.PITCH_FOV_HALF
+    discrete_yaw_offset = int(yaw_offset * 100000)
+    discrete_pitch_offset = int(pitch_offset * 100000)
+    packet += (discrete_yaw_offset & 0xFFFFFFFF).to_bytes(4, 'big')
+    packet += (discrete_pitch_offset & 0xFFFFFFFF).to_bytes(4, 'big')
+    # ENDING
+    packet += b'ED'
+    return packet
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture(gst_pipeline, cv2.CAP_GSTREAMER)
-
-    model = Yolo(MODEL_CFG_PATH, WEIGHT_PATH, META_PATH)
+    model = Yolo(config.MODEL_CFG_PATH, config.WEIGHT_PATH, config.META_PATH)
     aimer = Aim()
-    communicator = serial_port
+    # communicator = serial_port
     pkt_seq = 0
 
-    if cap.isOpened():
-        while True:
-            start = time.time()
-            ret, frame = cap.read()
-            if do_display:
-                cv2.imshow('CSI Camera',frame)
-                # This also acts as
-                keyCode = cv2.waitKey(30) & 0xff
-                # Stop the program on the ESC key
-                if keyCode == 27:
-                   break
+    rgbd_camera = config.RGBD_CAMERA(config.IMG_WIDTH, config.IMG_HEIGHT)
 
-            pred = model.detect(frame)
-            print('----------------\n',pred)
-            elapsed = time.time()-start
-            print('fps:',1./elapsed)
+    while True:
+        start = time.time()
+        frame, depth = rgbd_camera.get_frame()
 
-            # if len(pred) == 0:
-            #     packet = create_packet(SEARCH_TARGET, b"", pkt_seq)
-            # else:
-            #     hori_offset, vert_offset = aimer.get_rotation(pred)
-            #     hori_offset = hori_offset.to_bytes(4,'big')
-            #     vert_offset = vert_offset.to_bytes(4,'big')
-            #     packet = create_packet(MOVE_YOKE, hori_offset + vert_offset, pkt_seq)
-            # send_packet(communicator, packet)
+        pred = model.detect(frame)
+        print('----------------\n',pred)
+        elapsed = time.time()-start
+        print('fps:',1./elapsed)
 
-            # pkt_seq += 1
+        # FIXME: GET ENEMY TEAM FROM THE REFEREE SYS
+        enemy_team = 'blue'
 
-    else:
-        print('Failed to open camera!')
+        show_frame = frame.copy()
+        cv2.imshow('pred', show_frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            exit(0)
+        
+        yaw_diff, pitch_diff = aimer.process_one(pred, enemy_team, depth)
 
+        if pred:
+            packet = pad_packet(b'MY', pkt_seq, yaw_diff, pitch_diff)
+        else:
+            packet = pad_packet(b'ST', pkt_seq, 0, 0)
+        #send_packet(communicator, packet)
 
+        pkt_seq += 1
