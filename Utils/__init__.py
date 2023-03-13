29fbb4c8ab750069ddbcea2527c790baf7e4d293
RogerQi <rizhaoq2@illinois.edu>
2023-03-12 23:51:45 -0500

    Utils/ now conform to pydocstyle

--- Utils/__init__.py
+++ Utils/__init__.py
@@ -1,10 +1,10 @@
-# Commonly used utils functions
+"""Host commonly used utils functions."""
 import numpy as np
 
-
 def deg_to_rad(deg):
+    """Convert degree to radian."""
     return deg * ((2 * np.pi) / 360)
 
-
 def rad_to_deg(rad):
+    """Convert radian to degree."""
     return rad * (360. / (2 * np.pi))
