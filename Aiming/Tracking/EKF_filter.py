ea8e41374786355fa0b3c3b823e19f097e422f58
RogerQi <rizhaoq2@illinois.edu>
2023-06-23 13:59:10 -0500

    [chore] cleanup EKF filter impl

--- Aiming/Tracking/EKF_filter.py
+++ Aiming/Tracking/EKF_filter.py
@@ -4,13 +4,13 @@ import numpy as np
 class ExtendedKalmanFilter:
     def __init__(self):
         P0 = np.eye(9)
-        # P_post: 后验误差协方差矩阵，表示状态估计的不确定性
+        # Posteriori error covariance matrix, indicating the uncertainty of state estimation
         self.P_post = P0
-        # n: 系统的维度
+        # n: dimension of state vector
         self.n = P0.shape[0]
-        # I: 单位矩阵
+        # I: identity matrix for convenience
         self.I = np.eye(self.n)
-        # x_post: 后验状态，表示经过更新步骤后的状态估计值
+        # Posteriori state, indicating the state estimation after the update step
         self.x_post = np.zeros(self.n)
     
     def update_R(self, z):
@@ -117,23 +117,29 @@ class ExtendedKalmanFilter:
         self.F = self.get_jacobian_f(self.x_post, dt)
         self.Q = self.update_Q(dt)
 
-        self.x_pri = self.f(self.x_post)  # 通过状态转移函数进行预测
-        self.P_pri = self.F @ self.P_post @ self.F.T + self.Q  # 更新先验误差协方差矩阵
+        # Priori state estimate using state transition function
+        self.x_pri = self.f(self.x_post, dt) 
+        # Update priori error covariance matrix
+        self.P_pri = self.F @ self.P_post @ self.F.T + self.Q 
 
-        # 对下一次预测准备
+        # Update posteriori state and error covariance matrix for next iteration
         self.x_post = self.x_pri
         self.P_post = self.P_pri
 
         return self.x_pri
 
     def update(self, z, dt):
-        # 更新步骤
+        # Update state using observation
         self.H = self.get_jacobian_h(self.x_pri, dt)
         self.R = self.update_R(z)
 
-        # 计算卡尔曼增益
+        # Compute Kalman gain
         self.K = self.P_pri @ self.H.T @ np.linalg.inv(self.H @ self.P_pri @ self.H.T + self.R)
-        self.x_post = self.x_pri + self.K @ (z - self.h(self.x_pri))  # 用观测数据更新状态预测
-        self.P_post = (self.I - self.K @ self.H) @ self.P_pri  # 更新后验误差协方差矩阵
+
+        # Posteriori state estimate using observed data
+        self.x_post = self.x_pri + self.K @ (z - self.h(self.x_pri)) 
+
+        # Update posteriori error covariance matrix
+        self.P_post = (self.I - self.K @ self.H) @ self.P_pri
 
         return self.x_post
