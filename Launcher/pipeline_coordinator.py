9a4800e1c5e342d7cbcf741b7194fa497595c3c1
RogerQi <rizhaoq2@illinois.edu>
2023-07-03 14:54:11 -0500

    [chore] docs and linting for pipeline main and coordinator

--- Launcher/pipeline_coordinator.py
+++ Launcher/pipeline_coordinator.py
@@ -1,8 +1,25 @@
+"""Multi-stage multi-thread pipelinining."""
+
 import time
 import threading
 
+
 class pipeline_coordinator:
+    """Multi-stage multi-thread pipelinining.
+
+    The design is similar to a pipeline in CPU. The system is separated into
+    multiple stages, and each stage is executed in a separate thread. The
+    output of a stage is passed to the next stage.
+    """
+
     def __init__(self, stall_policy):
+        """Initialize the pipeline coordinator.
+
+        Args:
+            stall_policy: 'drop' or 'debug_keep_all'
+                - 'drop': drop all data except the latest one
+                - 'debug_keep_all': keep all data for debugging
+        """
         assert stall_policy in ['drop', 'debug_keep_all']
         self.stall_policy = stall_policy
 
@@ -17,8 +34,17 @@ class pipeline_coordinator:
         self.stage_dataflow_buffer = {}
 
         self.tic_tac_dict = {}
-    
+
     def register_pipeline(self, stage, func, name, input_list=[], output_list=[]):
+        """Register a pipeline stage.
+
+        Args:
+            stage(int): stage number
+            func(function): function to be executed in this stage
+            name(str): name of this stage
+            input_list(list): list of input variable names
+            output_list(list): list of output variable names
+        """
         if stage == 1:
             assert input_list == []
         assert stage not in self.stage_registration_dict.keys()
@@ -34,14 +60,23 @@ class pipeline_coordinator:
             'min_time': 999,
             'max_time': 0,
         }
-    
+
     def record_time(self, stage, time_elapsed):
+        """Record time for a stage for profiling.
+
+        Args:
+            stage(int): stage number
+            time_elapsed(float): time elapsed for this stage
+        """
         self.tic_tac_dict[stage]['total_time'] += time_elapsed
         self.tic_tac_dict[stage]['total_calls'] += 1
-        self.tic_tac_dict[stage]['min_time'] = min(self.tic_tac_dict[stage]['min_time'], time_elapsed)
-        self.tic_tac_dict[stage]['max_time'] = max(self.tic_tac_dict[stage]['max_time'], time_elapsed)
-    
+        self.tic_tac_dict[stage]['min_time'] = min(
+            self.tic_tac_dict[stage]['min_time'], time_elapsed)
+        self.tic_tac_dict[stage]['max_time'] = max(
+            self.tic_tac_dict[stage]['max_time'], time_elapsed)
+
     def parse_all_stage(self):
+        """Parse all stages and create threads."""
         self.max_stage = max(self.stage_registration_dict.keys())
         for stage_num in range(1, self.max_stage + 1):
             self.stage_dataflow_buffer[stage_num] = {
@@ -50,22 +85,27 @@ class pipeline_coordinator:
             }
             if stage_num == 1:
                 assert self.stage_registration_dict[stage_num]['inputs'] == []
+
                 def thread_func_factory(k):
                     while True:
                         start_cp = time.time()
                         # Pass through the function in the current stage
                         output_dict = self.stage_registration_dict[k]['func']()
                         if isinstance(output_dict, tuple):
-                            assert len(output_dict) == len(self.stage_registration_dict[k]['outputs'])
-                            output_dict = {k: v for k, v in zip(self.stage_registration_dict[k]['outputs'], output_dict)}
+                            assert len(output_dict) == len(
+                                self.stage_registration_dict[k]['outputs'])
+                            output_dict = {
+                                k: v for k, v in zip(
+                                    self.stage_registration_dict[k]['outputs'], output_dict)}
                         else:
                             assert len(self.stage_registration_dict[k]['outputs']) == 1
-                            output_dict = {self.stage_registration_dict[k]['outputs'][0]: output_dict}
+                            output_dict = {
+                                self.stage_registration_dict[k]['outputs'][0]: output_dict}
                         # Write output to the next stage
                         self.stage_dataflow_buffer[k]['lock'].acquire()
                         self.stage_dataflow_buffer[k]['data'].append(output_dict)
                         self.stage_dataflow_buffer[k]['lock'].release()
-                        time.sleep(0.001)
+                        time.sleep(0.001)  # TODO(roger): determine the best sleep time
                         end_cp = time.time()
                         self.record_time(k, end_cp - start_cp)
                 self.stage_thread_dict[stage_num] = threading.Thread(target=thread_func_factory,
@@ -73,16 +113,18 @@ class pipeline_coordinator:
             else:
                 def thread_func_factory(k):
                     while True:
-                        # Get output from previous stage
+                        # ===== Read input from the previous stage =====
                         self.stage_dataflow_buffer[k - 1]['lock'].acquire()
                         input_buffer = self.stage_dataflow_buffer[k - 1]['data']
                         if len(input_buffer) == 0:
+                            # If there is no data, release the lock and sleep
                             self.stage_dataflow_buffer[k - 1]['lock'].release()
                             # TODO(roger): use signal event for better call
                             time.sleep(0.001)
                             continue
                         else:
                             start_cp = time.time()
+                            # ===== Populate input data and handle buffer of last stage =====
                             if self.stall_policy == 'drop':
                                 # Keep only the latest data
                                 input_dict = input_buffer.pop(-1)
@@ -93,22 +135,26 @@ class pipeline_coordinator:
                             else:
                                 raise NotImplementedError
                             self.stage_dataflow_buffer[k - 1]['lock'].release()
-                            # Pass through the function in the current stage
+                            # ===== Pass through the function in the current stage =====
                             func_params_dict = {}
                             for var_name in self.stage_registration_dict[k]['inputs']:
                                 func_params_dict[var_name] = input_dict[var_name]
-                            output_vars = self.stage_registration_dict[k]['func'](**func_params_dict)
+                            output_vars = self.stage_registration_dict[k]['func'](
+                                **func_params_dict)
+                            # ===== Write output to the next stage =====
                             if k != self.max_stage:
                                 output_dict = {}
                                 if isinstance(output_vars, tuple):
-                                    assert len(output_vars) == len(self.stage_registration_dict[k]['outputs'])
+                                    assert len(output_vars) == len(
+                                        self.stage_registration_dict[k]['outputs'])
                                     for var_name in self.stage_registration_dict[k]['outputs']:
                                         output_dict[var_name] = output_vars.pop(0)
                                 elif output_vars is None:
                                     assert len(self.stage_registration_dict[k]['outputs']) == 0
                                 else:
                                     assert len(self.stage_registration_dict[k]['outputs']) == 1
-                                    output_dict[self.stage_registration_dict[k]['outputs'][0]] = output_vars
+                                    output_dict[self.stage_registration_dict[k]
+                                                ['outputs'][0]] = output_vars
                                 # Copy used input data for synchronization
                                 for input_k in input_dict.keys():
                                     assert input_k not in output_dict
@@ -120,15 +166,17 @@ class pipeline_coordinator:
                             end_cp = time.time()
                             self.record_time(k, end_cp - start_cp)
                 self.stage_thread_dict[stage_num] = threading.Thread(target=thread_func_factory,
-                                                             args=(stage_num, ))
+                                                                     args=(stage_num, ))
 
     def start(self):
+        """Start all threads."""
         for k in sorted(list(self.stage_thread_dict.keys())):
             self.stage_thread_dict[k].start()
         try:
             while True:
                 time.sleep(0.1)  # sleep forever
-        except KeyboardInterrupt:
+        except KeyboardInterrupt:  # TODO(roger): add exception handling to restart threads
+            # Profiling
             print("KeyboardInterrupt received, exiting...")
             print("Stage\tTotal\tCalls\tMin\tMax\tAvg")
             for k in sorted(list(self.tic_tac_dict.keys())):
--- pipelined_vision.py
+++ pipelined_vision.py
@@ -8,12 +8,13 @@ import config
 
 from Launcher.pipeline_coordinator import pipeline_coordinator
 
+
 def main():
     """Define the main while-true control loop that manages everything."""
     model = two_stage_yolo_detector(config, config.DEFAULT_ENEMY_TEAM)
     aimer = Aim(config)
 
-    assert config.DEBUG_DISPLAY == False, "display not supported in pipelined_vision.py"
+    assert not config.DEBUG_DISPLAY, "display not supported in pipelined_vision.py"
 
     communicator = UARTCommunicator(config)
     communicator.start_listening()
@@ -24,28 +25,32 @@ def main():
         print("OPENED SERIAL DEVICE AT: " + communicator.serial_port.name)
     else:
         print("SERIAL DEVICE IS NOT AVAILABLE!!!")
-    
+
     def read_from_comm_and_camera():
         stm32_state_dict = communicator.get_current_stm32_state()
         enemy_team = stm32_state_dict['enemy_color']
         model.change_target_color(enemy_team)
         frame = autoaim_camera.get_frame()
         return stm32_state_dict, enemy_team, frame
-    
+
     def tracker_and_viz(pred, enemy_team, resized_bgr_frame, stm32_state_dict):
         ret_dict = aimer.process_one(pred, enemy_team, resized_bgr_frame, stm32_state_dict)
         if ret_dict:
             communicator.process_one_packet(
                 config.MOVE_YOKE, ret_dict['abs_yaw'], ret_dict['abs_pitch'])
-    
+
     main_pipeline = pipeline_coordinator(stall_policy='debug_keep_all')
 
-    main_pipeline.register_pipeline(stage=1,
-                                    func=read_from_comm_and_camera,
-                                    name='STM32 State Update',
-                                    output_list=['stm32_state_dict', 'enemy_team', 'resized_bgr_frame'],
-                                    )
-    
+    main_pipeline.register_pipeline(
+        stage=1,
+        func=read_from_comm_and_camera,
+        name='STM32 State Update',
+        output_list=[
+            'stm32_state_dict',
+            'enemy_team',
+            'resized_bgr_frame'],
+    )
+
     main_pipeline.register_pipeline(stage=2,
                                     func=model.detect,
                                     name='YOLO Detection',
@@ -53,14 +58,20 @@ def main():
                                     output_list=['pred'],
                                     )
 
-    main_pipeline.register_pipeline(stage=3,
-                                    func=tracker_and_viz,
-                                    name='Tracking and Visualization',
-                                    input_list=['pred', 'enemy_team', 'resized_bgr_frame', 'stm32_state_dict'],
-                                    )
-    
+    main_pipeline.register_pipeline(
+        stage=3,
+        func=tracker_and_viz,
+        name='Tracking and Visualization',
+        input_list=[
+            'pred',
+            'enemy_team',
+            'resized_bgr_frame',
+            'stm32_state_dict'],
+    )
+
     main_pipeline.parse_all_stage()
     main_pipeline.start()
+
 
 if __name__ == "__main__":
     main()
